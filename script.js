7a571a47db0449405ee6eeafa976aa9194a3375f
jullima18 <julia.vilanova@aluno.senai.br>
2026-08-13 11:49:57 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const inputupload = document.getelementbyid('meu-upload')
 
+const url_imagem= ""
+const nome = ""
+
 inputupload.addeventlistener('change',function(evento) {
+
     const arquivo  = evento.target.files[0]
+
     if(arquivo) {
+        url_imagem = arquivo.name
         const nomedaimagem = arquivo.nome
 console.log(nomedaimagem)
     } else{
@@ -10,3 +16,17 @@ console.log(nomedaimagem)
     }
     
 });
+
+async function criarUsuario() {
+    const novoUsuario = {nome: 'ana', idade:25}
+    
+};
+const resposta = await fetch ('https://exemplo.com',{
+method: 'post',
+headers:{
+    'content-type':'application/json'
+};
+body: JSON.stringify(novoUsuario)
+});
+const resultado = await resposta.json();
+console.log ('criado com sucesso:',resultado)
